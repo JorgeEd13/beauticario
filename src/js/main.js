3a13610a25a3ee52511a36a87a35dfaaa9dbf4f8
Jorge <jorge.ed.ribeiro00@gmail.com>
2024-04-11 23:14:43 -0300

    Version 0.2 - Carousel Slider With Arrows

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,32 @@
 const timed__inputs = document.querySelectorAll('.timed__carousel__input');
-const timed__images = document.querySelectorAll('.promotion__carousel__img');
+// const timed__arrows = document.querySelectorAll('.timed__arrow');
+const timed__images = Array.from(document.querySelectorAll('.promotion__carousel__img'));
 
+// change image when directly selected
 timed__inputs.forEach((selection, index) => {
     selection.addEventListener('click', () => {
-        changeImage(index);
+        let selected__image = document.querySelector('.promotion__carousel__img__active');
+        let previousImageIndex = timed__images.indexOf(selected__image);
+
+        // if left arrow is clicked
+        if (index == timed__images.length && previousImageIndex > 0) {
+            selected__image.classList.remove('promotion__carousel__img__active');
+            timed__images[previousImageIndex - 1].classList.add('promotion__carousel__img__active');
+            timed__inputs[previousImageIndex - 1].checked = true;
+            
+            // if right arrow is clicked
+        } else if (index == timed__images.length + 1 && previousImageIndex < timed__images.length - 1) {
+            selected__image.classList.remove('promotion__carousel__img__active');
+            timed__images[previousImageIndex + 1].classList.add('promotion__carousel__img__active');
+            timed__inputs[previousImageIndex + 1].checked = true;
+
+        } else if (index >= 0 && index < timed__images.length) {
+            // removes 'active' status from previous image
+            selected__image.classList.remove('promotion__carousel__img__active');
+            // adds 'active' status to selected image
+            timed__images[index].classList.add('promotion__carousel__img__active');
+        }
+
+
     });
 });
-
-function changeImage(index) {
-        // removes 'active' status from previous image
-        const selected__image = document.querySelector('.promotion__carousel__img__active');
-        selected__image.classList.remove('promotion__carousel__img__active');
-        // adds 'active' status to selected image
-        timed__images[index].classList.add('promotion__carousel__img__active');
-};
