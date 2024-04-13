1efd7af3738af9bd2163498dcf7a163bdbc08bfb
Jorge <jorge.ed.ribeiro00@gmail.com>
2024-04-13 19:17:49 -0300

    Version 0.3 - User Carousel Slider Added

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,42 @@
+// gets timed carousel elements
 const timed__inputs = document.querySelectorAll('.timed__carousel__input');
-// const timed__arrows = document.querySelectorAll('.timed__arrow');
-const timed__images = Array.from(document.querySelectorAll('.promotion__carousel__img'));
+const timed__images = Array.from(document.querySelectorAll('.timed__carousel__img'));
 
-// change image when directly selected
-timed__inputs.forEach((selection, index) => {
+// gets user carousel
+// const user__slider = document.querySelector('.container__user__images');
+
+// const dragging = (e) => {
+//     console.log()
+// }
+
+// change image in timed carousel when directly selected
+timed__inputs.forEach((selection, inputIndex) => {
     selection.addEventListener('click', () => {
-        let selected__image = document.querySelector('.promotion__carousel__img__active');
-        let previousImageIndex = timed__images.indexOf(selected__image);
-
-        // if left arrow is clicked
-        if (index == timed__images.length && previousImageIndex > 0) {
-            selected__image.classList.remove('promotion__carousel__img__active');
-            timed__images[previousImageIndex - 1].classList.add('promotion__carousel__img__active');
-            timed__inputs[previousImageIndex - 1].checked = true;
-            
-            // if right arrow is clicked
-        } else if (index == timed__images.length + 1 && previousImageIndex < timed__images.length - 1) {
-            selected__image.classList.remove('promotion__carousel__img__active');
-            timed__images[previousImageIndex + 1].classList.add('promotion__carousel__img__active');
-            timed__inputs[previousImageIndex + 1].checked = true;
-
-        } else if (index >= 0 && index < timed__images.length) {
-            // removes 'active' status from previous image
-            selected__image.classList.remove('promotion__carousel__img__active');
-            // adds 'active' status to selected image
-            timed__images[index].classList.add('promotion__carousel__img__active');
-        }
-
-
+        changeImageTimed(inputIndex);
     });
 });
+
+// user__slider.addEventListener('mousemove', dragging);
+
+function changeImageTimed(index) {
+    let selected__image = document.querySelector('.timed__carousel__img__active');
+    let previousImageIndex = timed__images.indexOf(selected__image);
+
+    // if left arrow is clicked
+    if (index == timed__images.length && previousImageIndex > 0) {
+        selected__image.classList.remove('timed__carousel__img__active'); // removes 'active' status from previous image
+        timed__images[previousImageIndex - 1].classList.add('timed__carousel__img__active'); // adds 'active' status to new image
+        timed__inputs[previousImageIndex - 1].checked = true; // highlights correspondent input
+
+        // if right arrow is clicked
+    } else if (index == timed__images.length + 1 && previousImageIndex < timed__images.length - 1) {
+        selected__image.classList.remove('timed__carousel__img__active');
+        timed__images[previousImageIndex + 1].classList.add('timed__carousel__img__active');
+        timed__inputs[previousImageIndex + 1].checked = true;
+
+    } else if (index >= 0 && index < timed__images.length) {
+        selected__image.classList.remove('timed__carousel__img__active');
+        // adds 'active' status to selected image
+        timed__images[index].classList.add('timed__carousel__img__active');
+    }
+}
